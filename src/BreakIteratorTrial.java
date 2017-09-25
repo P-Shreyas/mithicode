82095ce19a6239fc2783143778a243912603ac7f
Shreyas Patankar <ssp1996@gmail.com>
2017-09-25 13:49:17 +0530

    Create class Existence to store page numbers

--- src/BreakIteratorTrial.java
+++ src/BreakIteratorTrial.java
@@ -2,36 +2,23 @@ import java.text.BreakIterator;
 public class BreakIteratorTrial {
 
     public static void main(String[] args) {
-        String sent = "Chapter 1 Introducing Bedework 3.6\n" +
-                "1.1\n" +
-                "Bedework System Overview\n" +
-                "Bedework is an open-source enterprise calendar system that supports public, personal, and\n" +
-                "group calendaring. It is designed to conform to current calendaring standards with a goal of\n" +
-                "attaining strong interoperability between other calendaring systems and clients. Bedework is\n" +
-                "built with an emphasis on higher education, though it is used by many commercial\n" +
-                "enterprises.\n" +
-                "You may choose to deploy Bedework for public events calendaring, personal calendaring and\n" +
-                "scheduling, or both. Bedework is suitable for embedding in other applications or in portals\n" +
-                "and has been deployed across a wide range of environments.\n" +
-                "Bedework System Architecture\n" +
-                "Bedework 3.6 has a central server architecture and is modular and extensible. It consists of\n" +
-                "the following components:\n" +
-                "• Bedework core calendar engine\n" +
-                "• Public events web client, called a “Calendar Suite”, for display of public events\n" +
-                "•\n" +
-                "•\n" +
-                "•\n" +
-                "•\n" +
-                "•\n" +
-                "Public events administration web client for entering public events, moderating\n" +
-                "pending events from the submissions client, and configuring calendar suites\n";
+        String sent = "and other resources.\n" +
+                "• TimeZone server for support of timezone information\n" +
+                "• Dump/Restore command-line utilities for database backup, initialization, and\n" +
+                "Mar 31, 2010\n" +
+                "G";
         BreakIterator bi = BreakIterator.getWordInstance();
         bi.setText(sent);
         int index = 0;
         while (bi.next() != BreakIterator.DONE) {
             String sentence = sent.substring(index, bi.current());
+            char c=sentence.charAt(0);
+            if (c> 65 && c < 92)
             System.out.println("Sentence: " + sentence);
             index = bi.current();
         }
+
+
+
     }
 }
---
+++ src/Existence.java
@@ -0,0 +1,37 @@
+public class Existence {
+    private boolean p1=false;
+    private boolean p2=false;
+    private boolean p3=false;
+
+
+    public void setP1()
+    {
+        this.p1=true;
+    }
+
+    public void setP2()
+    {
+        this.p2=true;
+    }
+
+    public void setP3()
+    {
+        this.p3=true;
+    }
+
+    public boolean getP1()
+    {
+        return this.p1;
+    }
+
+
+    public boolean getP2()
+    {
+        return this.p2;
+    }
+
+    public boolean getP3()
+    {
+        return this.p3;
+    }
+}
